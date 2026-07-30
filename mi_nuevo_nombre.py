9b3f6771e7d79c891dc5d5edaa51e9cbe55e4190
prxoman125 <gjeremygonzalez105@gmail.com>
2026-07-30 12:30:32 -0600

    Update mi_nuevo_nombre.py

--- mi_nuevo_nombre.py
+++ mi_nuevo_nombre.py
@@ -77,7 +77,7 @@ with col2:
                 texto = '#32CD32' 
             else:
                 color = '#8B0000'  
-                texto = '#FF7F7F' 
+                texto = '#B71C1C' 
             return f'background-color: {color}; color: {texto}; font-weight: bold;'
 
         # Aplicamos la función de estilo a la columna correspondiente
